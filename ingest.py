9ddd01f2ad854968c60831013e0c515659a4666a
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2021-06-28 18:13:14 -0500

    format download script

--- ingest.py
+++ ingest.py
@@ -1,11 +1,12 @@
 from kaggle.api.kaggle_api_extended import KaggleApi
 from kaggle.api_client import ApiClient
 from zipfile import ZipFile
+
 api = KaggleApi(ApiClient())
 api.authenticate()
 
-api.dataset_download_files('olistbr/brazilian-ecommerce')
+api.dataset_download_files("olistbr/brazilian-ecommerce")
 
-zf = ZipFile('./brazilian-ecommerce.zip')
-zf.extractall('brazilian-ecommerce')
+zf = ZipFile("./brazilian-ecommerce.zip")
+zf.extractall("brazilian-ecommerce")
 zf.close()
